fd37faa35a319a6fc88f1343a549edaf7ba5db62
Murad <mrdaydinov@gmail.com>
2024-04-09 12:44:41 +0400

    init commit

---
+++ ReflectionsLesson/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReflectionsLesson
+{
+    internal class Person
+    {
+        public int Age { get; set; }
+        public static int Increment { get; set; } = 0;
+
+        public Person()
+        {
+            Age += ++Increment;
+        }
+    }
+}
---
+++ ReflectionsLesson/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace ReflectionsLesson
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Person ali = new();
+            Console.WriteLine(ali.Age);
+
+            Person murad = new();
+            Console.WriteLine(murad.Age);
+
+            Person gadir = new();
+            Console.WriteLine(gadir.Age);
+
+            Person nicat = new();
+            Console.WriteLine(nicat.Age);
+        }
+    }
+}
